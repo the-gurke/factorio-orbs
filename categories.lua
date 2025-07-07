03f86e7597e3a0e43600c28ca16e41cf8211ccfd
the-gurke <spam@leike.name>
2025-07-06 22:08:37 -0700

    add soul collection

--- categories.lua
+++ categories.lua
@@ -1,7 +1,64 @@
 -- categories.lua
--- Define the new crafting categories and recipe groups
+-- Define new item groups and subgroups
 
--- Create the orbs recipe category
+-- Main item group for Orbs
+data:extend({
+  {
+    type = "item-group",
+    name = "orbs",
+    order = "z",
+    icon = "__orbs__/graphics/orb-initial-technology.png",
+    icon_size = 1024
+  }
+})
+
+-- Subgroups for organizing items within the Orbs group
+data:extend({
+  {
+    type = "item-subgroup",
+    name = "orbs-manifest",
+    group = "orbs",
+    order = "a"
+  },
+  {
+    type = "item-subgroup",
+    name = "orbs-conjure",
+    group = "orbs",
+    order = "b"
+  },
+  {
+    type = "item-subgroup",
+    name = "orbs-proliferate",
+    group = "orbs",
+    order = "c"
+  },
+  {
+    type = "item-subgroup",
+    name = "souls",
+    group = "orbs",
+    order = "d"
+  },
+  {
+    type = "item-subgroup",
+    name = "divination",
+    group = "orbs",
+    order = "e"
+  },
+  {
+    type = "item-subgroup",
+    name = "orbs-research",
+    group = "orbs",
+    order = "f"
+  },
+  {
+    type = "item-subgroup",
+    name = "orbs-machines",
+    group = "orbs",
+    order = "g"
+  }
+})
+
+-- Recipe categories
 data:extend({
   {
     type = "recipe-category",
@@ -9,61 +66,10 @@ data:extend({
   }
 })
 
--- Create the custom orb-speed module category
+-- Module categories for orb modules
 data:extend({
   {
     type = "module-category",
     name = "orb-speed"
-  }
-})
-
--- Create the orbs item group
-data:extend({
-  {
-    type = "item-group",
-    name = "orbs",
-    order = "z",
-    icon = "__orbs__/graphics/magic-orb.png",
-    icon_size = 1024
-  }
-})
-
--- Create subgroups for organizing recipes within the orbs tab
-data:extend({
-  {
-    type = "item-subgroup",
-    name = "orbs-conjure",
-    group = "orbs",
-    order = "a"
-  },
-  {
-    type = "item-subgroup",
-    name = "orbs-proliferate",
-    group = "orbs",
-    order = "b"
-  },
-  {
-    type = "item-subgroup",
-    name = "orbs-manifest",
-    group = "orbs",
-    order = "c"
-  },
-  {
-    type = "item-subgroup",
-    name = "divination",
-    group = "orbs",
-    order = "d"
-  },
-  {
-    type = "item-subgroup",
-    name = "orbs-research",
-    group = "orbs",
-    order = "e"
-  },
-  {
-    type = "item-subgroup",
-    name = "orbs-machines",
-    group = "orbs",
-    order = "f"
   }
 })
