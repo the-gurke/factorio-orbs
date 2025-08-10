4bc36f26624bfb1c05198ec29a418a368e4dd038
the-gurke <thegurke@proton.me>
2025-08-09 19:18:52 -0700

    new rune chain logic with a dead end at umbra/mortis

--- rune-chains.lua
+++ rune-chains.lua
@@ -5,12 +5,13 @@ local rune_transformation_chains = {
   ["rune-word-aqua"] = {"rune-word-aqua", "rune-word-aqua", "rune-word-aqua", "rune-word-ventus"},
   ["rune-word-ventus"] = {"rune-word-aqua", "rune-word-aqua", "rune-word-ventus", "rune-word-aqua", "rune-word-ignis"},
   ["rune-word-ignis"] = {"rune-word-aqua", "rune-word-ventus", "rune-word-terra", "rune-word-lux"},
-  ["rune-word-terra"] = {"rune-word-ventus", "rune-word-ignis", "rune-word-lux", "rune-word-ventus", "rune-word-aqua", "rune-word-umbra"},
-  ["rune-word-lux"] = {"rune-word-ventus", "rune-word-ventus", "rune-word-ignis", "rune-word-aqua", "rune-word-ventus"},
-  ["rune-word-umbra"] = {"rune-word-mortis", "rune-word-tempus", "rune-word-terra"},
-  ["rune-word-tempus"] = {"rune-word-tempus", "rune-word-tempus", "rune-word-tempus", "rune-word-tempus", "rune-word-vitae", "rune-word-tempus", "rune-word-mortis", "rune-word-ventus"},
-  ["rune-word-vitae"] = {"rune-word-tempus", "rune-word-vitae"},
-  ["rune-word-mortis"] = {"rune-word-ignis", "rune-word-tempus", "rune-word-ventus", "rune-word-umbra", "rune-word-tempus", "rune-word-umbra"}
+  ["rune-word-terra"] = {"rune-word-ventus", "rune-word-ignis", "rune-word-tempus", "rune-word-ventus", "rune-word-aqua", "rune-word-umbra"},
+  ["rune-word-tempus"] = {"rune-word-tempus", "rune-word-tempus", "rune-word-tempus", "rune-word-tempus", "rune-word-lux", "rune-word-tempus", "rune-word-umbra", "rune-word-ventus"},
+  ["rune-word-umbra"] = {"rune-word-umbra", "rune-word-umbra", "rune-word-umbra", "rune-word-mortis"},
+  ["rune-word-mortis"] = {"rune-word-mortis", "rune-word-umbra", "rune-word-ventus"},
+  ["rune-word-lux"] = {"rune-word-ventus", "rune-word-ventus", "rune-word-ignis", "rune-word-aqua", "rune-word-ventus", "rune-word-vitae"},
+  ["rune-word-vitae"] = {"rune-word-tempus", "rune-word-vitae"}
+
 }
 
 return rune_transformation_chains
