10a805cab2a03aca0434fb1c78918d3b1f506430
boeseMilch <boeseMilch@thisisnotanemail.com>
2025-10-17 23:59:45 -0700

    make hiding the research tree a mod setting

---
+++ settings.lua
@@ -0,0 +1,12 @@
+-- settings.lua
+-- Define mod settings
+
+data:extend({
+  {
+    type = "bool-setting",
+    name = "orbs-hide-research-tree",
+    setting_type = "startup",
+    default_value = true,
+    order = "a"
+  }
+})
